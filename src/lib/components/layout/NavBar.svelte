788d3dc87f7fddd4586adfb3511b232ff17281ce
grayson40 <graysoncrozier40@gmail.com>
2024-03-21 21:29:01 -0400

    Navbar changes

--- src/lib/components/layout/NavBar.svelte
+++ src/lib/components/layout/NavBar.svelte
@@ -2,8 +2,8 @@
 	import { Button } from '$lib/components/ui/button';
 	import Logo from '$lib/client/images/tps.png';
 	import MobileLogo from '$lib/client/images/tps.png';
-	import { CldImage } from 'svelte-cloudinary';
 	import { cartLengthStore } from '$lib/client/cart';
+	import * as Menubar from "$lib/components/ui/menubar";
 	export let user: {
 		firstName: string;
 		lastName: string;
@@ -11,21 +11,12 @@
 		isAdmin: boolean;
 	} | null;
 
-	const handleCollectionsHover = () => {
-		document.getElementById('drop-menu')?.classList.remove('hidden');
-	};
-
 	const handleMobileMenu = () => {
 		const menu = document.getElementById('mobile-nav');
 		menu?.classList.remove('opacity-0');
 		menu?.classList.remove('pointer-events-none');
 		menu?.classList.add('opacity-100');
 	};
-
-	export let pieces: {
-		id: string;
-		name: string;
-	}[];
 </script>
 
 <nav
@@ -38,64 +29,32 @@
 		</svg>
 	</button>
 
-	<div
-		class="h-[400px] bg-white absolute drop-shadow-md border-t-[1px] border-solid border-neutral-300 w-full -mx-12 top-[78px] text-center z-50 grid grid-cols-3 hidden no-scroll"
-		id="drop-menu"
-	>
-		<div class="col-span-1 w-full overflow-hidden">
-			<!-- CLD IMAGE SWITCH NEEDED -->
-			<CldImage
-				width={400}
-				src={'retro-logo' || 'https://via.placeholder.com/355x200'}
-				alt="Description of my image"
-				class="shadow-md"
-				style="object-fit: contain;"
-			/>
-		</div>
-		<div class="flex flex-col p-6 overflow-y-scroll">
-			<div class="px-4 py-0 rounded-md text-gray-500 font-extralight text-sm">Pieces</div>
-			{#each pieces as piece}
-				<a
-					href={`/products/${piece.id}`}
-					class="px-4 py-3 rounded-md hover:text-gray-600 cursor-pointer font-jura">{piece.name}</a
-				>
-			{/each}
-		</div>
-		<div class="flex flex-col p-6 overflow-y-scroll">
-			<div class="px-4 py-0 rounded-md text-gray-500 font-extralight text-sm">Collections</div>
-			<a href="/products" class="px-4 py-3 rounded-md hover:text-gray-600 cursor-pointer font-jura"
-				>All Pieces</a
-			>
-			<a
-				href="/products?tag=Illuminate"
-				class="px-4 py-3 rounded-md hover:text-gray-600 cursor-pointer font-jura"
-				>Illuminate Collection</a
-			>
-		</div>
-	</div>
-
 	<a class="text-4xl font-light mx-auto sm:mx-0" href="/">
 		<img src={Logo} alt="TPG" class=" h-[70px] hidden sm:flex" />
 		<img src={MobileLogo} alt="TPG" class="h-[70px] sm:hidden flex" />
 	</a>
 
 	<div class="flex-row items-center hidden sm:flex gap-6">
-		<a
-			href="/products"
-			class="text-black uppercase font-jura flex flex-row items-center"
-			on:mouseenter={handleCollectionsHover}
-		>
-			Collections
-			<svg
-				width="24"
-				height="45"
-				viewBox="0 0 24 55"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path d="M7 21.7705L12 33.2288L17 21.7705H7Z" fill="black" />
-			</svg>
-		</a>
+		<Menubar.Root>
+			<Menubar.Menu>
+				<Menubar.Trigger>
+					<button class="text-black uppercase font-jura flex flex-row items-center"> Collections </button>
+				</Menubar.Trigger>
+				<Menubar.Content>
+					<a href="/products">
+						<Menubar.Item>
+							All Products
+						</Menubar.Item>
+					</a>
+				<Menubar.Separator />
+				<a href="/products?tag=Illuminate">
+					<Menubar.Item>
+						Illuminate
+					</Menubar.Item>
+				</a>
+				</Menubar.Content>
+			</Menubar.Menu>
+		</Menubar.Root>
 		<a href="/about/faq" class="text-black uppercase font-jura flex flex-row items-center"> FAQ </a>
 		{#if user}
 			<a href="/profile">
